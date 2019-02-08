3d4846b405bfcc63dadf59bb82a731ab72ae806e
Jo-JeongMuk <45927377+Jo-JeongMuk@users.noreply.github.com>
2019-02-08 09:07:16 +0900

    Add files via upload

--- src/PointToPoint_List.cpp
+++ src/PointToPoint_List.cpp
@@ -2,39 +2,50 @@
 #include "PointToPoint_List.h"
 #include <cmath>
 #include <cstdlib>
+#include <list>//-
 
 #define LOCA_PUB    "bebop/states/ardrone3/PilotingState/PositionChanged"
 #define ALTI_PUB    "bebop/states/ardrone3/PilotingState/AltitudeChanged"
 #define ATTI_PUB    "bebop/states/ardrone3/PilotingState/AttitudeChanged"
 #define PI          3.14159265
-#define LIMITE_DIST 1.5
+#define LIMITE_DIST 3.5
 
 PointToPoint::PointToPoint(int argc, char** argv)
-: init_argc(argc), init_argv(argv), homeOrNot(true)
-{
-
+: init_argc(argc), init_argv(argv), homeOrNot(true), pause(false) {
 }
 
-void PointToPoint::init()
-{
+void PointToPoint::init() {
     ros::init(init_argc, init_argv, "PointToPoint_List");
     ros::NodeHandle ptopnh;
     ptop_pub = ptopnh.advertise<geometry_msgs::Twist>("bebop/cmd_vel", 1);
+    takeoff = ptopnh.advertise<std_msgs::Empty>("bebop/takeoff", 1);
+    land = ptopnh.advertise<std_msgs::Empty>("bebop/land", 1);
+    altitude = ptopnh.subscribe
+            (ALTI_PUB, 1, &PointToPoint::altitudeCallback, this);
+    attitude = ptopnh.subscribe
+            (ATTI_PUB, 1, &PointToPoint::attitudeCallback, this);
     location = ptopnh.subscribe
                (LOCA_PUB, 1, &PointToPoint::locationCallback, this);
-    altitude = ptopnh.subscribe
-               (ALTI_PUB, 1, &PointToPoint::altitudeCallback, this);
-    attitude = ptopnh.subscribe
-               (ATTI_PUB, 1, &PointToPoint::attitudeCallback, this);
 }
 
-void PointToPoint::locationCallback(const LOCATION)
-{
-    if (this->homeOrNot == true)
-    {
+void PointToPoint::altitudeCallback(const ALTITUDE) {
+    this->c_alti = msg->altitude;
+//    ROS_INFO("hight : %lf\n", this->c_alti);
+}
+
+void PointToPoint::attitudeCallback(const ATTITUDE) {
+    this->c_yaw = msg->yaw;
+//    ROS_INFO("yaw : %lf ", this->c_yaw);
+}
+
+void PointToPoint::locationCallback(const LOCATION) {
+    if (this->homeOrNot == true) {
         this->h_lati    = msg->latitude;
         this->h_long    = msg->longitude;
         this->homeOrNot = false;
+        if (this->c_alti == 0.0)
+            takeoff.publish(this->empty_msg);
+        sleep(3000);
     }
 
     this->c_lati = msg->latitude;
@@ -42,29 +53,31 @@ void PointToPoint::locationCallback(const LOCATION)
 //    ROS_INFO("%lf , %lf\n", this->c_lati, this->c_long);
 }
 
-void PointToPoint::altitudeCallback(const ALTITUDE)
-{
-    this->c_alti = msg->altitude;
-//    ROS_INFO("hight : %lf\n", this->c_alti);
+void PointToPoint::Pause() {
+    while (this->pause) {
+        PointToPoint::Stop();
+        sleep(500);
+    }
 }
 
-void PointToPoint::attitudeCallback(const ATTITUDE)
-{
-    this->c_yaw = msg->yaw;
-//    ROS_INFO("yaw : %lf ", this->c_yaw);
+void PointToPoint::Stop() {
+    this->twist.linear.x = 0;
+    this->twist.linear.y = 0;
+    this->twist.linear.z = 0;
+    this->twist.angular.x = 0;
+    this->twist.angular.y = 0;
+    this->twist.angular.z = 0;
+    this->ptop_pub.publish(twist);
 }
 
 void PointToPoint::Move(List* list) {
-
     List* Current = list;
-    while(Current != NULL)
-    {
+    while(Current != NULL) {
         double g_lati = Current->Latitude;
         double g_long = Current->Longitude;
         double g_alti = Current->Altitude;
         double speed  = Current->Speed;
-        while(true)
-        {
+        while(true) {
             try {
                 ros::spinOnce();
             } catch (...) {
@@ -72,11 +85,14 @@ void PointToPoint::Move(List* list) {
                 ros::shutdown();
             }
 
+            if (this->pause == true)
+                PointToPoint::Pause();
+
             float linXYZ_AngZ[4] = {0, 0, 0, 0};
             double c_lati_radian = this->c_lati * (PI / 180);
             double c_long_radian = this->c_long * (PI / 180);
-            double g_lati_radian = g_lati * (PI / 180);
-            double g_long_radian = g_long * (PI / 180);
+            double g_lati_radian =       g_lati * (PI / 180);
+            double g_long_radian =       g_long * (PI / 180);
 
             double dist = (((acos(sin(c_lati * PI / 180.0) * sin(g_lati * PI / 180.0) +
                     cos(c_lati * PI / 180.0) * cos(g_lati * PI / 180.0) * cos((c_long - g_long) *
@@ -89,28 +105,45 @@ void PointToPoint::Move(List* list) {
                     / (cos(c_lati_radian) * sin(dist_rad)));
 
             double true_bearing = radian * (180 / PI);
-            double c_bearing    = (this->c_yaw * 180 / PI) + 60;
+            double c_bearing    = (this->c_yaw * 180 / PI);
 
-//            if (sin(g_long_radian - c_long_radian) < 0) true_bearing = true_bearing + 180;
+////////////////////////////////////////////////////////////////////////////////////////////
+
+            if (sin(g_long_radian - c_long_radian) < 0) {
+                true_bearing = 360 - true_bearing;
+                c_bearing = c_bearing + 65;
+            }
+            if (sin(g_long_radian - c_long_radian) > 0) {
+                c_bearing = c_bearing + 65;
+            }
+
+///////////////////////////////////////////////////////////////////////////////////////////
 
             if (c_bearing <  0  ) c_bearing = 360 + c_bearing;
-            if (c_bearing >= 360) c_bearing = 360 - c_bearing;
+            if (c_bearing >= 360) c_bearing = c_bearing - 360;
+
+            if (true_bearing <  0  ) true_bearing = 360 + true_bearing;
+            if (true_bearing >= 360) true_bearing = true_bearing - 360;
 
             if (this->c_alti < g_alti - 0.3)
                 linXYZ_AngZ[2]  = 0.2;
             else if (this->c_alti > g_alti + 0.3)
-                linXYZ_AngZ[2]  = -0.2;
+                linXYZ_AngZ[2]  = - 0.2;
             else linXYZ_AngZ[2] = 0;
 
-            if (true_bearing < c_bearing + 10 && dist > LIMITE_DIST)
-                linXYZ_AngZ[3]  = 0.15;
-            else if (true_bearing > c_bearing - 10 && dist > LIMITE_DIST)
-                linXYZ_AngZ[3]  = -0.15;
-            else linXYZ_AngZ[3] = 0;
+            if(sin(g_long_radian - c_long_radian) > 0 || dist > 7.0) {
+                if (true_bearing < c_bearing + 10 && dist > LIMITE_DIST)
+                    linXYZ_AngZ[3] = 0.1;
+                else if (true_bearing > c_bearing - 10 && dist > LIMITE_DIST)
+                    linXYZ_AngZ[3] = -0.1;
+                else
+                    linXYZ_AngZ[3] = 0;
+            }
 
-            if (true_bearing < c_bearing + 10 && true_bearing > c_bearing - 10 && dist > LIMITE_DIST)
+            if (dist < 7.0 || true_bearing < c_bearing + 10 && true_bearing > c_bearing - 10 && dist > LIMITE_DIST)
                 linXYZ_AngZ[0]  = 0.5 * speed;
-            else linXYZ_AngZ[0] = 0;
+            else
+                linXYZ_AngZ[0] = 0.005;
 
             this->twist.linear.x  = linXYZ_AngZ[0];
             this->twist.linear.y  = linXYZ_AngZ[1];
@@ -120,45 +153,52 @@ void PointToPoint::Move(List* list) {
             this->twist.angular.z = linXYZ_AngZ[3];
             this->ptop_pub.publish(twist);
 
-            ROS_INFO("goal : %.2lf , Current : %.2lf, dist : %.2lf", true_bearing, c_bearing, dist);
-            if (dist < LIMITE_DIST && Current->next != NULL)
-            {
+//            ROS_INFO("goal : %.2lf , Current : %.2lf, dist : %.2lf", true_bearing, c_bearing, dist);
+            if (dist < LIMITE_DIST && Current->next != NULL) {
                 Current = Current->next;
                 break;
             }
-            else if (dist < LIMITE_DIST && Current->next == NULL &&  g_lati != this->h_lati && g_long != this->h_long)
-            {
+            else if (dist < LIMITE_DIST && Current->next == NULL &&  g_lati != this->h_lati && g_long != this->h_long) {
                 g_lati = this->h_lati;
                 g_long = this->h_long;
+            }
+            else if (dist < LIMITE_DIST && g_lati == this->h_lati && g_long == this->h_long) {
+                PointToPoint::Stop();
+                sleep(3000);
+                land.publish(this->empty_msg);
+                return ;
             }
         }
     }
 }
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
 
     List* list;//
     list = new List;//
     list->Latitude = 36.519547;// 운동장 서남쪽
     list->Longitude = 127.172806;//
-    list->Altitude = 2.0;//
-    list->Speed = 0.2;//
+    list->Altitude = 2;//
+    list->Speed = 0.5;//
     list->next = new List;//
     list->next->Latitude = 36.519686;// 운동장 동남쪽
     list->next->Longitude = 127.173387;//
-    list->next->Altitude = 2.0;//
-    list->next->Speed = 0.2;//
+    list->next->Altitude = 2;//
+    list->next->Speed = 0.5;//
     list->next->next = NULL;//
+
+//    36.519896;//
+//    127.1728021; 본관 정면 운동장//
 
     PointToPoint a(argc, argv);
     a.init();
     a.Move(list);
 
-    while(list)//
-    {//
+    while(list) {//
         List* tmp = list;//
         list = list->next;//
         delete tmp;//
     }//
+
     return 0;
 }
